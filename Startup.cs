8bcc64be9bfc046871ea34a70915d48f159e0aed
TakaoKoshi <koshi@digitaldolphins.jp>
2021-06-18 19:20:50 +0900

    ファイルの追加

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServerSignalRApp.Server.Hubs;
 
 namespace BlazorServerTest
 {
@@ -29,11 +31,18 @@ namespace BlazorServerTest
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
 			services.AddSingleton<WeatherForecastService>();
+			services.AddResponseCompression(opts =>
+			{
+				opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+					new[] { "application/octet-stream" });
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			app.UseResponseCompression();
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
@@ -53,6 +62,7 @@ namespace BlazorServerTest
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapBlazorHub();
+				endpoints.MapHub<ChatHub>("/chathub");
 				endpoints.MapFallbackToPage("/_Host");
 			});
 		}
